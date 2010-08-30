eb830408007cb8fd27e67ea06e28c10eb2ac5a0f
Ken Gullaksen <ken@glxn.net>
2010-08-30 15:36:20 +0200

    set alwaysontop to false on main frame (closes #13)

--- src/main/java/net/glxn/jmsutility/JMSUtility.java
+++ src/main/java/net/glxn/jmsutility/JMSUtility.java
@@ -43,6 +43,7 @@ public class JMSUtility extends Component {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        frame.setAlwaysOnTop(false);
     }
 
     public JMSUtility() {
